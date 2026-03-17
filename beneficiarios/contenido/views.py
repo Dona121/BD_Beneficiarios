0bd0147f921f8d17de774db1dc2a8a8a89e3cb07
Dona121 <Carrascal544@gmail.com>
2026-03-17 10:50:25 -0500

    Agregada Paginacion

--- beneficiarios/contenido/views.py
+++ beneficiarios/contenido/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from contenido import models
+from django.core.paginator import Paginator
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 
 def formulario(request):
-    personas = models.Persona.objects.all()
+    todas_las_personas = models.Persona.objects.all()
+    paginator = Paginator(todas_las_personas,2)
+    numero_pagina = request.GET.get('page',1)
+    numero_pagina = int(numero_pagina)
+
+    if numero_pagina < 1:
+        numero_pagina = 1
+    elif numero_pagina > paginator.num_pages:
+        numero_pagina = paginator.num_pages
+        
+    pagina = paginator.page(numero_pagina)
+
     return render(
         request,
         'templates/formulario.html',
-        {"personas":personas}
+        {"personas":pagina.object_list,"pagina":numero_pagina}
     )
